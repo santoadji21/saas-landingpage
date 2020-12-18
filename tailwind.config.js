d44cfe87737d9aa970e09c8b58effd5740517fcf
santoadji21 <ahmad.aji.santoso21@gmail.com>
2020-12-18 13:28:22 +0700

    Initial Commmit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      orange: '#FF7143',
+      darkblue: '#5454D4',
+      black: '#19191B',
+      gray: '#696871',
+      white: '#FFFFFF',
+      transparent: 'transparent',
+      graylight: '#E7E7E7',
+      redblack:'#9F3919'
+    },
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
